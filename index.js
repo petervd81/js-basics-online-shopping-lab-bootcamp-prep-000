f2a69e4f0ee87ad1defd131e2b528e784f376150
Peter van Dijk <peter@petervandijk.net>
2018-05-19 21:31:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber.isNaN) {
+  if (cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var totalCost = total();
